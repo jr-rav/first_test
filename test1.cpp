cf93f2f359e96a8889d6d9187723fee541f2e931
Jaimin <JR@example.com>
2019-04-14 16:22:10 -0400

    Adding test2.cpp and modified test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
-  cout << "Hello World and universe and galaxy" << endl;
+  cout << "Hello World and universe and galaxy and everything" << endl;
+  cout << "Hey" << endl;
   return 0;
 }
